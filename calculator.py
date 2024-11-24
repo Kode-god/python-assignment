24a2babc576929a008f85eba12d04d3048dc94d6
Kevin2.0 <163003196+Kode-god@users.noreply.github.com>
2024-11-24 13:45:45 +0300

    my first python assignment

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Function to perform mathematical operation
+def perform_operation(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error! Division by zero."
+    else:
+        return "Invalid operation!"
+
+# User inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform oeration and display the results
+result = perform_operation(num1, num2, operation)
+print(f"{num1} {operation} {num2} = {result}")
